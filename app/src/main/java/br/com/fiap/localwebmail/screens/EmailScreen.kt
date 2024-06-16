0d2f2a2e808623ac32cd9b10759235b2e3c25dce
Kaue Meurer <kauemluiz@gmail.com>
2024-06-16 18:27:39 -0300

    Estilizacao do app

--- app/src/main/java/br/com/fiap/localwebmail/screens/EmailScreen.kt
+++ app/src/main/java/br/com/fiap/localwebmail/screens/EmailScreen.kt
@@ -1,13 +1,21 @@
 package br.com.fiap.localwebmail.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import br.com.fiap.localwebmail.model.emails
 
@@ -23,9 +31,29 @@ fun EmailScreen(navController: NavController) {
         }
     )
     { innerPadding ->
-        LazyColumn(contentPadding = innerPadding) {
-            items(emails) { email ->
-                EmailItem(email, navController)
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+//                .background(Color.DarkGray)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Seus e-mails", style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+
+                Column {
+                    LazyColumn(contentPadding = innerPadding) {
+                        items(emails) { email ->
+                            EmailItem(email, navController)
+                        }
+                    }
+                }
             }
         }
     }
