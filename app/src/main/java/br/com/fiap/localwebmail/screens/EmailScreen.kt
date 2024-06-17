add05babe9fbd75bdaf3a1d09bb823e47202bbae
Kaue Meurer <kauemluiz@gmail.com>
2024-06-17 16:51:01 -0300

    Menu configuracao

--- app/src/main/java/br/com/fiap/localwebmail/screens/EmailScreen.kt
+++ app/src/main/java/br/com/fiap/localwebmail/screens/EmailScreen.kt
@@ -1,5 +1,6 @@
 package br.com.fiap.localwebmail.screens
 
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,22 +9,36 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import br.com.fiap.localwebmail.model.emails
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmailScreen(navController: NavController) {
-
-
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Seus e-mails") }, // Title next to the menu
+                actions = { // Actions on the right side
+                    IconButton(onClick = { navController.navigate("emailpage") }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = { navController.navigate("emailpage") }) {
                 Icon(imageVector = Icons.Filled.Add, contentDescription = "Novo E-mail")
@@ -37,6 +52,10 @@ fun EmailScreen(navController: NavController) {
                 .fillMaxSize()
 //                .background(Color.DarkGray)
         ) {
+
+
+
+
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -45,6 +64,9 @@ fun EmailScreen(navController: NavController) {
                     text = "Seus e-mails", style = MaterialTheme.typography.titleLarge,
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
+
+
+
 
 
                 Column {
